41e8511ea0dda892cfdc610bacea86b4f05812c6
Jacob Wiltse <jacob.wiltse@mariadb.com>
2022-03-23 10:51:04 -0400

    bump required local provider version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws        = ">= 3.3.0"
-    local      = ">= 1.4"
+    local      = ">= 2.2"
     null       = ">= 2.1"
     template   = ">= 2.1"
     random     = ">= 2.1"
